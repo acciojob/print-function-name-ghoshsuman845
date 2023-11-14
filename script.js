7405d2b743b37b32fc59be98617e06812d2db5b3
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-11-14 13:38:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
-function functionName() {
-  alert(arguments.callee.name);
+function printFunctionName() {
+  // Get the name of the function dynamically using arguments.callee.name
+  const functionName = arguments.callee.name;
+  
+  console.log(`The name of this function is: ${functionName}`);
 }
 
-functionName();
+// Calling the function
+printFunctionName(); // Output: The name of this function is: printFunctionName
